b48917bcc80853bc44b1710f0202631152959b08
Jay <dunnpoker98@gmail.com>
2022-12-13 20:33:58 +1100

    Update vanity.py

--- vanity.py
+++ vanity.py
@@ -1,8 +1,8 @@
-import web3
+from web3 import Web3
 import re
 
 # define pattern to search for in address
-pattern = "^0x1[a-zA-Z0-9]{3,}$"
+pattern = "^0x420DAB"
 
 # connect to Ethereum network
 web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io'))
@@ -13,11 +13,14 @@ while True:
     priv_key = web3.eth.account.create().privateKey
 
     # compute public key and address
-    pub_key = web3.eth.account.privateKeyToAccount(priv_key).publicKey
-    address = web3.eth.account.privateKeyToAccount(priv_key).address
+    account = web3.eth.account.privateKeyToAccount(priv_key)
+    pub_key = account.address
+    address = account.address
 
     # check if address matches pattern
     if re.match(pattern, address):
-        # output matching address
-        print(address)
+        # output matching address and private key as hex strings
+        print(f"Address: 0x{hex(int(address, 16))[2:]}")
+        print(f"Private Key: 0x{hex(int(priv_key.hex(), 16))[2:]}")
         break
+
