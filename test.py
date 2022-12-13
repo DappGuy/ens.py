4e9b09104acb25e486741c9874d286d68e87df82
Jay <dunnpoker98@gmail.com>
2022-12-13 19:27:35 +1100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+from web3 import Web3
+from eth_ens import ENS
+import time
+
+# set up web3 instance and connect to Ethereum network
+web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io'))
+
+# set up ENS instance
+ens = ENS(web3)
+
+# define current time and time 24 hours from now
+now = time.time()
+expiration = now + (24 * 60 * 60)
+
+# get all ENS domains that expire within 24 hours
+owner = ens.owner()
+
+# filter owner's domains by expiration
+expiring_domains = filter(lambda domain: domain.expiry == expiration, owner.domains)
+
+print(expiring_domains)
+
