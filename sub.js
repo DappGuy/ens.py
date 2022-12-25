f78fd51cb37e816edeaee4364e1f4088e0b58389
Jay <dunnpoker98@gmail.com>
2022-12-25 18:24:19 +1100

    Update sub.js

--- sub.js
+++ sub.js
@@ -1,1 +1,15 @@
+const { ApiPromise } = require('@polkadot/api');
 
+async function main() {
+  // Connect to a remote Substrate node
+  const api = await ApiPromise.create({
+    provider: 'wss://dev.diora.network'
+  });
+
+  console.log('Connected to the Substrate node!');
+
+  // Disconnect from the node when the script is finished
+  api.disconnect();
+}
+
+main().catch(console.error);
